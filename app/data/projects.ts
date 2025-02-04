0390f0045d04c6e1e3c0c875619d7da295e33467
Harsha <harshasai52439@gmail.com>
2025-02-04 13:16:43 +0530

    Updated portfolio

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -17,7 +17,7 @@ export const projects = [
       "This project involved developing a fast-paced multiplayer shooter game using Core Games. The game features free-for-all gameplay, a variety of weapons, and dynamic maps designed to keep players engaged. The development process focused on creating fluid mechanics, balanced gameplay, and visually appealing environments, resulting in an exciting and competitive multiplayer experience.",
     technologies: ["Core Games"],
     image: "/Screenshot 2025-01-24 095558.png",
-    onlineLink: "https://example.com/carnage-clash",
+    onlineLink: "https://www.coregames.com/games/5f4c43/carnage-clash",
   },
   {
     id: 3,
@@ -27,7 +27,7 @@ export const projects = [
       "This project involved designing a ride-hailing mobile application inspired by Uber, focusing on optimizing the user experience. The goal was to create a seamless and intuitive app for ride booking, real-time tracking, and payments. The design process included extensive user flow mapping, wireframing, and prototyping to ensure a user-friendly interface. The final product resulted in a clean and efficient design that simplified the user journey.",
     technologies: ["Figma", "Google Material Design"],
     image: "/original-9ca9dce6717fac418c149b85e23c0ed9.webp",
-    onlineLink: "https://example.com/uber-ux-design",
+    onlineLink: "https://www.figma.com/proto/srh9wNZcEdbrrZntuGhxbj/Uber?page-id=0%3A1&node-id=11-13&p=f&viewport=-2525%2C626%2C0.5&t=buN5J7vNt74LqFXF-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=8%3A2&show-proto-sidebar=1",
   },
 ]
 
